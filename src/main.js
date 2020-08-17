4f97f83d6c22da0b47b157f0c3fcb28b5bc5bba8
wsz987 <51685914+wsz987@users.noreply.github.com>
2020-08-17 21:37:21 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
